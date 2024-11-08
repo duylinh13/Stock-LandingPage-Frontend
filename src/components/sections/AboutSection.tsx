821f7348b8d33d949c30681af03f0b04349e0893
duylinh13 <huynhleduylinh1311@gmail.com>
2024-11-08 23:00:06 +0700

    fix:delete img

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -1,49 +1,36 @@
-import BTC from "../../assets/BTC.png";
 import Button from "../Button";
 
 const AboutSection = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center py-8 mx-4 sm:mx-6 md:mx-8 lg:mx-12 xl:mx-16">
-      {/* Text Section */}
-      <div className="w-full md:w-1/2 px-4 md:px-6 lg:px-10 mb-6 md:mb-0">
-        <p className="text-xl sm:text-3xl md:text-4xl lg:text-5xl text-blue-800 font-bold">
+    <div className=" py-16 px-4 sm:px-8 lg:px-16 text-center">
+      <div className="max-w-2xl mx-auto space-y-4">
+        <p className="text-2xl sm:text-4xl text-blue-800 font-bold">
           One platform
         </p>
-        <p className="text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-3 sm:mb-4 lg:mb-6">
-          all things Stocks
+        <p className="text-2xl sm:text-4xl font-bold mb-4 sm:mb-6">
+          All Things Stocks Smart Investment
         </p>
-        <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold text-green-950 mb-3 sm:mb-5">
-          Smart Investation
-        </h3>
-        <p className="text-gray-600 mb-2 sm:mb-3 lg:mb-4 text-sm sm:text-base lg:text-lg">
+
+        <p className="text-gray-600 text-lg">
           Invest Your Funds with Us and Enjoy Astonishing Returns
         </p>
-        <p className="text-gray-600 mb-4 sm:mb-6 text-sm sm:text-base lg:text-lg">
+        <p className="text-gray-500 text-md">
           Make the most of your investment in the world of capital markets by
-          taking <br className="hidden sm:block" />
+          taking <br className="hidden md:inline" />
           advantage of Maven's many features and conveniences.
         </p>
-        <div className="flex space-x-2 sm:space-x-3">
-          <div className="my-6 sm:my-8">
+        <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-6">
+          <div>
             <Button
               label="Get Started"
               variant="primary"
-              className="shadow-lg shadow-blue-500/50 transform translate-y-1"
+              className="px-6 py-2 rounded-full"
             />
           </div>
-          <button className="text-blue-700 font-bold py-2 px-4 rounded hover:underline transition duration-300 text-sm sm:text-base lg:text-lg">
+          <button className="text-blue-600 font-medium hover:underline">
             See How it Works
           </button>
         </div>
-      </div>
-
-      {/* Image Section */}
-      <div className="w-full md:w-1/2 flex justify-center">
-        <img
-          src={BTC}
-          alt="Bitcoin logo"
-          className="w-48 sm:w-64 md:w-80 lg:w-[500px] h-auto"
-        />
       </div>
     </div>
   );
